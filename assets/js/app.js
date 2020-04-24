76f8431c2befa7d709079c1b9bea56dfc837fadb
Fchelo <frarecinos@gmail.com>
2020-04-23 19:57:12 -0600

    2th commit adding tasks from textbox to the lis and addi the list at the DOM

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+//variables //vars
+const listaTweets = document.getElementById('lista-tweets')
+
+
+
+
+
+
+//Event Listeners
+
+eventListeners();
+function eventListeners(){
+    //cuando se envia el formulario // when form is send
+    document.querySelector("#formulario").addEventListener('submit',agregarTweet);
+}
+
+//Funciones
+
+
+//Añadir tweet del formulario
+function agregarTweet(e){
+    e.preventDefault();
+    //leer el valor de text tarea // taking the value of text tarea
+    console.log('Formulario enviado')
+    const tweet = document.getElementById('tweet').value
+    //crear boton de eliminar // create delet butto
+    const botonBorrar = document.createElement('a')
+    botonBorrar.classList = 'borrar-tweet'
+    botonBorrar.innerText = 'X'
+    //crear el contenido y añadir el contenido a la lista
+    // crate the content and add the content at the list
+    const li = document.createElement('li')
+    li.innerText = tweet
+    li.appendChild(botonBorrar)
+    //agregando la lista al DOM //add list to the DOM
+    listaTweets.appendChild(li)
+    console.log(tweet);
+
+}
